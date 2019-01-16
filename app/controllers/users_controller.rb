2640b64e8c3cceb7ef2d53c4971f100605ccd3a6
NickSot <nikola.s.sotirov@gmail.com>
2019-01-16 10:15:11 +0200

    Added user login system(very primitive)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+	def index
+
+	end
+
+	def login
+		u = User.where user_params
+
+		if u == []
+			@message = "Credentials invalid!"
+		else
+			@message = "Successfully logged!"
+		end
+
+		render 'index'
+	end
+
+	def new
+
+	end
+
+	def create
+		User.create user_params
+
+		render 'index'
+	end
+
+	def user_params
+		return params.require(:user).permit(:email, :password)
+	end
+end
